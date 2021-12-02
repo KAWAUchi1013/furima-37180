e02cab7da5447983adc4a1097b66f6bbac48b928
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-12-02 16:06:39 +0900

    ユーザー管理機能１０

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :name, presence: true,format: {with:/\A[ぁ-んァ-ン一-龥々]+\z/}
+         validates :name, presence: true,format: {with:/\A[ぁ-んァ-ン一-龥々ー]+\z/}
          validates :first_name_reading, presence: true,format: {with:/\A[ァ-ヶー－]+\z/}
          validates :birthday, presence: true
          validates :nickname, presence: true
-         validates :surname, presence: true,format: {with:/\A[ぁ-んァ-ン一-龥々]+\z/}
+         validates :surname, presence: true,format: {with:/\A[ぁ-んァ-ン一-龥々ー]+\z/}
          validates :name_reading, presence: true,format: {with:/\A[ァ-ヶー－]+\z/}
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
