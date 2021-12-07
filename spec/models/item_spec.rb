94090cd8f584efcd0429b2adbf661d8c091259c3
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-12-07 20:15:01 +0900

    商品出品機能８

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,57 +19,57 @@ RSpec.describe Item, type: :model do
          expect(@item.errors.full_messages).to include "Image can't be blank"
        end
         it "カテゴリーの情報が必須であること" do
-          @item.category_id= ''
+          @item.category_id= nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Category can't be blank"
         end
         it "商品の状態の情報が必須であること" do
-          @item.product_condition_id = ''
+          @item.product_condition_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Product condition can't be blank"
         end
         it"配送料の負担の情報が必須であること" do
-          @item.burden_of_shipping_charge_id = ''
+          @item.burden_of_shipping_charge_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Burden of shipping charge can't be blank"
         end
         it "発送元の地域の情報が必須であること" do
-          @item.shipping_area_id= ''
+          @item.shipping_area_id= nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Shipping area can't be blank"
         end
         it "発送までの日数の情報が必須であること" do
-          @item.days_to_ship_id = ''
+          @item.days_to_ship_id = nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Days to ship can't be blank"
         end
         it "価格の情報が必須であること" do
-          @item.product_price = ''
+          @item.product_price = nil
           @item.valid?
           expect(@item.errors.full_messages).to include "Product price can't be blank"
         end
         it "カテゴリーに「---」が選択されている場合は出品できない" do
-          @item.category_id = '0'
+          @item.category_id = 0
           @item.valid?
           expect(@item.errors.full_messages).to include "Category must be other than 0"
         end
         it "商品の状態に「---」が選択されている場合は出品できない" do
-          @item.product_condition_id  = '0'
+          @item.product_condition_id  = 0
           @item.valid?
           expect(@item.errors.full_messages).to include "Product condition must be other than 0"
         end
         it "配送料の負担に「---」が選択されている場合は出品できない" do
-          @item.burden_of_shipping_charge_id = '0'
+          @item.burden_of_shipping_charge_id = 0
           @item.valid?
           expect(@item.errors.full_messages).to include "Burden of shipping charge must be other than 0"
         end
         it "発送元の地域に「---」が選択されている場合は出品できない" do
-          @item.shipping_area_id = '0'
+          @item.shipping_area_id = 0
           @item.valid?
           expect(@item.errors.full_messages).to include "Shipping area must be other than 0"
         end
         it "発送までの日数に「---」が選択されている場合は出品できない" do
-          @item.days_to_ship_id = '0'
+          @item.days_to_ship_id = 0
           @item.valid?
           expect(@item.errors.full_messages).to include "Days to ship must be other than 0"
         end
