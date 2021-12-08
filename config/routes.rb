0758a8f05c24d777b49fc9965e9b6183302336a0
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-12-08 12:00:18 +0900

    商品一覧表示機能１

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
+  resources :items, only: [:new, :create] do
+    resources :items, only: [:index, :create]
+  end
 end
