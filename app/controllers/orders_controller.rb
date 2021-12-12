a1a58d9e010414ff559be72a1ab20fde84def37b
KAWAUchi1013 <93497980+KAWAUchi1013@users.noreply.github.com>
2021-12-12 14:17:21 +0900

    商品購入機能２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-
+  before_action :itemes_params, only: [:create,:index]
   def index
     @order_address = BuyerOrder.new
   end
@@ -7,21 +7,27 @@ class OrdersController < ApplicationController
   def create
     @order_address = BuyerOrder.new(order_params)
     if @order_address.valid?
-      # pay_item
+       pay_item
       @order_address.save
       return redirect_to root_path
     end
-  end  
+  end 
+
   
    private
 
    def order_params
-    params.require(:buyer_order).permit(:price,:address, :postal_code, :municipalities, :building_name, :telephone_number, :shipping_area_id).merge(user_id: current_user.id,item_id:  params[:item_id])
+    params.require(:buyer_order).permit(:address, :postal_code, :municipalities, :building_name, :telephone_number, :shipping_area_id).merge(user_id: current_user.id,token: params[:token],item_id:  params[:item_id])
    end
+
+   def itemes_params
+    @item = Item.find(params[:item_id])
+  end
+
     def pay_item
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        amount: order_params[:price],  # 商品の値段
+        amount: @item.product_price,  # 商品の値段
         card: order_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
--- app/models/buyer_order.rb
+++ app/models/buyer_order.rb
@@ -1,22 +1,17 @@
 class BuyerOrder
 
   include ActiveModel::Model
-  attr_accessor :price, :address,:postal_code, :municipalities, :building_name, :telephone_number, :buyer, :shipping_area_id,:user_id,:item_id
+  attr_accessor :token,:price,:address,:postal_code, :municipalities, :building_name, :telephone_number, :buyer, :shipping_area_id,:user_id,:item_id
 
-  # validates :price,presence: true
   validates :postal_code,presence: true
   validates :municipalities,presence: true
-  # validates :building_name,presence: true
   validates :telephone_number,presence: true
-  # validates :buyer,presence: true
+  validates :item_id,presence: true
   validates :shipping_area_id,presence: true
   validates :user_id,presence: true
-  validates :item_id,presence: true
+  validates :token,presence: true
   def save
-      # 寄付情報を保存し、変数donationに代入する
       buyer = Buyer.create(item_id: item_id, user_id: user_id)
-      # 住所を保存する
-      # donation_idには、変数donationのidと指定する
       Order.create(address: address, postal_code: postal_code, municipalities: municipalities, building_name: building_name, telephone_number: telephone_number,buyer_id: buyer.id,shipping_area_id: shipping_area_id)
     end
 
